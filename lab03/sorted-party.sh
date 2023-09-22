a650b07462f79da7c466e249cad28aed48001c51
mo95 <144040446+corewater95@users.noreply.github.com>
2023-09-21 23:16:08 -0400

    Update sorted-party.sh

--- lab03/sorted-party.sh
+++ lab03/sorted-party.sh
@@ -1,5 +1,38 @@
 
 #!/bin/bash
+
+
+if [ $# -ne 1 ]; then
+  echo " $0 <filename>"
+  
+fi
+
+if [ ! -f "$1" ]; then
+  echo "Error: File '$1' does not exist."
+  
+fi
+
+if [[ "$1" != *.txt ]]; then
+  echo "Error: File extension not allowed."
+  
+fi
+
+sort "$1" > sorted.txt
+
+echo "Sorting completed."
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo "filename: "$@ red='\033[1;31m'
 
 if [[ $@ =~ [-t]$ ]]; then
